7fc11e32c8abd3095e4024c6e235f92853aae298
Brian Yip <itsbriany@gmail.com>
2015-03-22 11:22:18 -0300

    The boost libraries can now be built

--- libs/buildLibs.sh
+++ libs/buildLibs.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+DIR=$(pwd)
+
 # A quick script to prepare the libraries
 git clone https://github.com/boostorg/boost.git
-cd boost
 git submodule init
 git submodule update
-./booststrap.sh
+
+# Install b2
+cd $DIR/boost
+./bootstrap.sh
+
 # Now run ./b2 --show-libraries to see libraries that can be installed
 # Run ./b2 --with-<library> to install the library
+./b2 --with-system
 
-
+# Cleanup
+cd ..
+mv bootstrap.log boost/
