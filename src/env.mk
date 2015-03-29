f2baaa6c765419f655da92782611b63638814749
Brian Yip <itsbriany@gmail.com>
2015-03-28 22:25:01 -0300

    Create env.mk

---
+++ src/env.mk
@@ -0,0 +1,5 @@
+# Set these variables according to your dev environment
+
+# Examples
+INCLUDES=-I/home/itsbriany/workbench/libs/boost_1_57_0/
+LINKDIR=-L/home/itsbriany/workbench/libs/boost_1_57_0/stage/lib
