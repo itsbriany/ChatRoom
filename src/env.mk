f525320aeb248916ca8c0bf4d02bea11a1d8dad0
Brian Yip <itsbriany@gmail.com>
2015-03-28 21:26:46 -0300

    gitignore test

--- src/env.mk
+++ src/env.mk
@@ -1,5 +1,5 @@
 # Set these variables according to your dev environment
 
 # Examples
-INCLUDES=-I/root/workbench/libs/boost_1_57_0/
-LINKDIR=-L/root/workbench/libs/boost_1_57_0/stage/lib
+INCLUDES=-I/home/itsbriany/workbench/libs/boost_1_57_0/
+LINKDIR=-L/home/itsbriany/workbench/libs/boost_1_57_0/stage/lib
