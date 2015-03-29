2fa472c0b936148434098b2f70af6d7931f15b97
Brian Yip <itsbriany@gmail.com>
2015-03-28 23:28:19 -0300

    Delete env.mk

--- src/env.mk
+++
@@ -1,5 +0,0 @@
-# Set these variables according to your dev environment
-
-# Examples
-INCLUDES=-I/home/itsbriany/workbench/libs/boost_1_57_0/
-LINKDIR=-L/home/itsbriany/workbench/libs/boost_1_57_0/stage/lib
